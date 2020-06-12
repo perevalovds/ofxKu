2d73911c1ef7b74372d2b4dc99f3ecb01ce9073a
perevalovds <perevalovds@gmail.com>
2020-06-12 13:55:20 +0500

    fix crc32 for non-existing file

--- src/ofxKuUtilsCrc32.cpp
+++ src/ofxKuUtilsCrc32.cpp
@@ -25,14 +25,19 @@ unsigned int ofxKuUtilsCrc32(void *buffer, unsigned long len)
 unsigned int ofxKuUtilsCrc32(const std::string &file_name, bool use_data_path) {
 	unsigned long len = ofxKuFileSize(file_name, use_data_path);
 	vector<char> buffer(len);
+//--------------------------------------------------------------
+unsigned int ofxKuUtilsCrc32(const std::string &file_name, bool use_data_path) {
+	unsigned long len = ofxKuFileSize(file_name, use_data_path);
 	if (len > 0) {
 		string file1 = file_name;
 		if (use_data_path) file1 = ofToDataPath(file1);
 		ifstream f(file1, std::ios::binary);
+		vector<char> buffer(len);
 		f.read(&buffer[0], len);
+		//f.gcount() - number of bytes
+		return ofxKuUtilsCrc32(&buffer[0], len);
 	}
-	//f.gcount() - number of bytes
-	return ofxKuUtilsCrc32(&buffer[0], len);
+	return 0;
 }
 
 //--------------------------------------------------------------
