ec10dd61be1097287ea0ee18bcc1c054ba35c994
perevalovds <perevalovds@gmail.com>
2021-09-12 23:21:51 +0500

    added matrix for drawing aligned fitted image

--- src/ofxKuDrawRasters.cpp
+++ src/ofxKuDrawRasters.cpp
@@ -37,3 +37,48 @@ void ofxKuOfGrayPixelsToRaster(ofPixels &pixels, vector<unsigned char> &pix, int
 }
 
 //--------------------------------------------------------------
+//Sets drawing matrix for drawing aligned images, fit in a given rectangle; image can be scaled using "scale"
+//"align" = 0 - center, -1 - top/left, 1 - right/bottom
+//usage:
+//	ofPushMatrix(); 
+//	ofxKuDrawImageSetMatrixFit(...);
+//	tex.draw(0,0); 
+//	ofPopMatrix();
+
+
+void ofxKuDrawImageSetMatrixFit(float img_w, float img_h, ofRectangle rect_to_fit, int align_x, int align_y, float scale) {
+	float X = rect_to_fit.getLeft();
+	float Y = rect_to_fit.getTop();
+		
+	float W = rect_to_fit.getWidth();
+	float H = rect_to_fit.getHeight();
+
+	if (W <= 0 || H <= 0 || img_w <= 0 || img_h <= 0) {
+		cout << "ofxKuDrawImagePushMatrixFit - empty image or negative params" << endl;
+		return;
+	}
+
+	//fit size
+	float scl = min(W / img_w, H / img_h) * scale;
+	float resw = img_w * scl;
+	float resh = img_h * scl;
+	float x = 0;	//left
+	float y = 0;	//top
+	if (align_x == 0) {
+		x = (W - resw) / 2;
+	}
+	if (align_x == 1) {
+		x = W - resw;
+	}
+	if (align_y == 0) {
+		y = (H - resh) / 2;
+	}
+	if (align_y == 1) {
+		y = H - resh;
+	}
+	ofTranslate(X + x, Y + y);		//<------ Translating
+	ofScale(scl, scl);
+
+}
+
+//--------------------------------------------------------------
--- src/ofxKuDrawRasters.h
+++ src/ofxKuDrawRasters.h
@@ -9,3 +9,12 @@ void ofxKuDrawRaster(vector<float> &pix, int w, int h, float X, float Y, float W
 	bool nearest_filter = true);
 
 void ofxKuOfGrayPixelsToRaster(ofPixels &pixels, vector<unsigned char> &pix, int &w, int &h);
+
+//ofPushMatrix for drawing aligned images, fit in a given rectangle; image can be scaled using "scale"
+//"align" = 0 - center, -1 - top/left, 1 - right/bottom
+//usage:
+//	ofPushMatrix(); 
+//	ofxKuDrawImageSetMatrixFit(...);
+//	tex.draw(0,0); 
+//	ofPopMatrix();
+void ofxKuDrawImageSetMatrixFit(float img_w, float img_h, ofRectangle rect_to_fit, int align_x = 0, int align_y = 0, float scale=1);
