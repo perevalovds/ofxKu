11822e2b3cd1a4f8db0417f984effe0f760a45c1
Denis Perevalov <perevalovds@gmail.com>
2024-01-07 21:43:16 +0300

    debugbrk

--- src/kuErrors.h
+++ src/kuErrors.h
@@ -4,10 +4,10 @@
 #include "ofMain.h"
 
 #define kuAssertWarning( val, message ) \
-{ if (!(val)) { cout << "WARNING: " << message << endl; ofSystemAlertDialog( message ); } }
+{ if (!(val)) { cout << "WARNING: " << message << endl; ofSystemAlertDialog( message ); __debugbreak(); } }
 
 #define kuAssertCritical( val, message ) \
-{ if (!(val)) { cout << "CRITICAL ERROR: " << message << endl; ofSystemAlertDialog( message ); OF_EXIT_APP(0); } }
+{ if (!(val)) { cout << "CRITICAL ERROR: " << message << endl; ofSystemAlertDialog( message ); __debugbreak(); OF_EXIT_APP(0); } }
 
 #define kuExit( message ) \
 { cout << "EXITING: " << message << endl; ofSystemAlertDialog( message ); OF_EXIT_APP(0); }
