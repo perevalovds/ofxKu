886eaf06b763772f521f5773f60021c6fff65774
Denis Perevalov <perevalovds@gmail.com>
2024-01-07 21:47:44 +0300

    fix debug break

--- src/kuErrors.h
+++ src/kuErrors.h
@@ -3,8 +3,19 @@
 
 #include "ofMain.h"
 
+#ifdef _DEBUG
+#define KU_DEBUG_BREAK __debugbreak();
+#else
+#define KU_DEBUG_BREAK {}
+#endif
+
 #define kuAssertWarning( val, message ) \
-{ if (!(val)) { cout << "WARNING: " << message << endl; ofSystemAlertDialog( message ); __debugbreak(); } }
+{ if (!(val)) { \
+    cout << "WARNING: " << message << endl; \
+    ofSystemAlertDialog( message ); \
+    KU_DEBUG_BREAK \
+    } \
+}
 
 #define kuAssertCritical( val, message ) \
 { if (!(val)) { cout << "CRITICAL ERROR: " << message << endl; ofSystemAlertDialog( message ); __debugbreak(); OF_EXIT_APP(0); } }
